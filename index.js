ed7aa03b273273325e5080edffd32a7457961670
jfcann <57755717+jfcann@users.noreply.github.com>
2020-02-12 23:54:19 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-d3.select('h1').style('color', '#95360A');
+/*d3.select('h1').style('color', '#95360A')
+               .style('font-family': 'monospace')
+               .style('font-color': '#95360A')
+               .style('transform': translate(100, 100))*/
 
 var svgWidth = 500, svgHeight = 500;
 var svg = d3.select("svg")
@@ -11,3 +14,10 @@ var circle = svg.append("circle")
     .attr("cy", 250)
     .attr("r", 250)
     .attr("fill", "#0F5B84");
+
+var user = svg.append("text")
+    .text("jfcann)
+    .attr("font-family", "monaco")
+    .attr("y", 250)
+    .attr("x", 250)
+    .attr("fill", "#95360A");
